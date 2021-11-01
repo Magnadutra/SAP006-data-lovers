2b9da4232baa30bd3e0a0889f7c1adaab85482ff
Magna Dutra <magdutra.jobs@gmail.com>
2021-11-01 12:10:54 -0300

    ajustes cod

--- src/data.js
+++ src/data.js
@@ -24,25 +24,3 @@ export const calcFilter = (dataBase, selectedFilter) =>
 
 
 
-
-
-
-// export const filterSelected = (dataBase, valueGenderSelected, propriedade) =>
-//   dataBase.filter((dataBase) => dataBase[propriedade] === valueGenderSelected);
-
-// export const sortOrder = (dataBase, valueSpeciesSelected) => {
-//   const order = dataBase.sort((a, b) => (a.name < b.name ? -1 : a.name > b.name ? 1 : 0));
-//   switch (valueSpeciesSelected) {
-//     case "az":
-//       return order
-//     case "za":
-//       return order.reverse()
-//   }
-// };
-// export const calcFilter = (dataBase, selectedFilter) =>
-//   parseFloat((selectedFilter.length * 100) / dataBase.length).toFixed(2);
-
-
-
-
-  
